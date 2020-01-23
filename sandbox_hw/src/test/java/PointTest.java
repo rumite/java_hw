49dccc0773b49669dabcab77c0eb359c8ebd617c
Bakhtiyarov, Timur <timur.bakhtiyarov@x5.ru>
2020-01-23 10:10:47 +0300

    enable testng

---
+++ sandbox_hw/src/test/java/PointTest.java
@@ -0,0 +1,18 @@
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ru.stqa.pft.sandbox_hw.Point;
+
+import java.util.Set;
+
+import static ru.stqa.pft.sandbox_hw.Point.distance;
+
+public class PointTest {
+    @Test
+    public void testPointDistance() {
+        Point p1 = new Point(2, 4);
+        Point p2 = new Point(-1, 10);
+        Assert.assertEquals(distance(p1,p2), 0);
+        //Assert.assertEquals(distance(p1,p2), 0);
+    }
+}
